527229b6090c71d1ae8c915fd47f1d7d7369b64d
Khanhvd <eros.yun711@gmail.com>
2025-05-08 13:40:58 +0700

    Sửa lại authen elastic

--- vector_db/chroma_db.py
+++
@@ -1,7 +0,0 @@
-import chromadb
-
-class ChromaDB:
-    def __init__(self, base_dir):
-        self.base_dir = base_dir
-        self.chroma_client = chromadb.PersistentClient(path="./chroma_db")
-        self.collection = self.chroma_client.get_or_create_collection(name="data_training")
